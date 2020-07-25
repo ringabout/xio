57c0429b6dd8318dc3d792a9934d57ac26203fa6
flywind <xzsflywind@gmail.com>
2020-07-25 16:43:32 +0800

    cleaan

--- src/xio/iokit/iocp.nim
+++ src/xio/iokit/iocp.nim
@@ -11,8 +11,16 @@ type
   HDC* = Handle
   HGLRC* = Handle
 
+
+const
+  INVALID_HANDLE_VALUE = -1
+
 proc createIoCompletionPort*(FileHandle: Handle, ExistingCompletionPort: Handle,
-                             CompletionKey: ULONG_PTR, NumberOfConcurrentThreads: DWORD):
-                             Handle {.libKernel32, importc: "CreateIoCompletionPort"}
+  CompletionKey: ULONG_PTR, NumberOfConcurrentThreads: DWORD
+): Handle {.libKernel32, importc: "CreateIoCompletionPort"}
   ## Creates an input/output (I/O) completion port and associates it with a specified file handle
 
+
+
+when isMainModule:
+  echo createIoCompletionPort(INVALID_HANDLE_VALUE, 0, 0, 1)
