69e6098ac3a85e89e4d37c596a45f7731f4fcf0b
flywind <xzsflywind@gmail.com>
2020-07-25 17:22:52 +0800

    done

--- src/xio/iokit/iocp.nim
+++ src/xio/iokit/iocp.nim
@@ -5,11 +5,31 @@ type
   Handle* = int
   DWORD* = int32
   PDWORD* = ptr DWORD
+  LPDWORD* = ptr DWORD
   LPINT* = ptr int32
   ULONG_PTR* = uint
   PULONG_PTR* = ptr uint
   HDC* = Handle
   HGLRC* = Handle
+  PVOID* = pointer
+
+  WINBOOL = int32 ## if WINBOOL != 0, it succeeds which is different from posix.
+
+  OVERLAPPED_offset* = object
+    offset*: DWORD
+    offsetHigh*: DWORD
+
+  OVERLAPPED_union* {.union.} = object
+    offset*: OVERLAPPED_offset
+    p*: PVOID
+
+  OVERLAPPED* = object
+    internal*: ULONG_PTR
+    internalHigh*: ULONG_PTR
+    union*: OVERLAPPED_union
+    hevent*: Handle
+
+  LPOVERLAPPED* = ptr OVERLAPPED
 
 
 const
@@ -18,8 +38,15 @@ const
 proc createIoCompletionPort*(FileHandle: Handle, ExistingCompletionPort: Handle,
   CompletionKey: ULONG_PTR, NumberOfConcurrentThreads: DWORD
 ): Handle {.libKernel32, importc: "CreateIoCompletionPort"}
-  ## Creates an input/output (I/O) completion port and associates it with a specified file handle
+  ## Creates an input/output (I/O) completion port and associates it with a specified file handle.
 
+
+proc getQueuedCompletionStatus*(CompletionPort: Handle,
+  lpNumberOfBytesTransferred: LPDWORD,
+  lpCompletionKey: PULONG_PTR,
+  lpOverlapped: LPOVERLAPPED,
+  dwMilliseconds: DWORD
+): WinBool {.libKernel32, importc: "GetQueuedCompletionStatus"}
 
 
 when isMainModule:
