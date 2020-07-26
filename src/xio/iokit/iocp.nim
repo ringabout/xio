fd31049cc8a2d366739d79895dea009c4263da3e
flywind <xzsflywind@gmail.com>
2020-07-26 17:03:59 +0800

    partly win api

--- src/xio/iokit/iocp.nim
+++ src/xio/iokit/iocp.nim
@@ -1,4 +1,5 @@
 {.pragma: libKernel32, stdcall, dynlib: "Kernel32.dll".}
+{.pragma: libWs2_32, stdcall, dynlib: "Ws2_32.dll".}
 
 
 type
@@ -7,11 +8,13 @@ type
   PDWORD* = ptr DWORD
   LPDWORD* = ptr DWORD
   LPINT* = ptr int32
+  ULONG* = int32
   ULONG_PTR* = uint
   PULONG_PTR* = ptr uint
   HDC* = Handle
   HGLRC* = Handle
   PVOID* = pointer
+  LPVOID* = pointer
 
   WINBOOL = int32 ## if WINBOOL != 0, it succeeds which is different from posix.
 
@@ -32,6 +35,27 @@ type
   LPOVERLAPPED* = ptr OVERLAPPED
 
 
+  WSAOVERLAPPED* = object
+    internal*: ULONG_PTR
+    internalHigh*: ULONG_PTR
+    offset*: DWORD
+    offsetHigh*: DWORD
+    hevent*: Handle
+
+  LPWSAOVERLAPPED* = ptr WSAOVERLAPPED
+
+  LPWSAOVERLAPPED_COMPLETION_ROUTINE* = proc (dwError: DWORD, cbTransferred: DWORD, 
+                                             lpOverlapped: LPWSAOVERLAPPED, dwFlags: DWORD)
+
+  WSABUF* = object
+    len*: ULONG
+    buf*: cstring
+
+  LPWSABUF* = ptr WSABUF
+
+  SocketHandle* = distinct int
+
+
 const
   INVALID_HANDLE_VALUE = -1
 
@@ -40,7 +64,6 @@ proc createIoCompletionPort*(FileHandle: Handle, ExistingCompletionPort: Handle,
 ): Handle {.libKernel32, importc: "CreateIoCompletionPort"}
   ## Creates an input/output (I/O) completion port and associates it with a specified file handle.
 
-
 proc getQueuedCompletionStatus*(CompletionPort: Handle,
   lpNumberOfBytesTransferred: LPDWORD,
   lpCompletionKey: PULONG_PTR,
@@ -48,6 +71,26 @@ proc getQueuedCompletionStatus*(CompletionPort: Handle,
   dwMilliseconds: DWORD
 ): WinBool {.libKernel32, importc: "GetQueuedCompletionStatus"}
 
+proc closeHandle*(hObject: Handle): WINBOOL {.libKernel32,
+    importc: "CloseHandle".}
+
+proc WSAIoctl*(s: SocketHandle; dwIoControlCode: DWORD; lpvInBuffer: LPVOID;
+  cbInBuffer: DWORD; lpvOutBuffer: LPVOID; cbOutBuffer: DWORD;
+  lpcbBytesReturned: LPDWORD; lpOverlapped: LPOVERLAPPED;
+  lpCompletionRoutine: LPWSAOVERLAPPED_COMPLETION_ROUTINE
+): cint {.libWs2_32, importc: "WSAIoctl".}
+
+proc WSARecv*(
+  s: SocketHandle,
+  lpBuffers: LPWSABUF,
+  dwBufferCount: DWORD,
+  lpNumberOfBytesRecvd: LPDWORD,
+  lpFlags: LPDWORD,
+  lpOverlapped: LPWSAOVERLAPPED,
+  lpCompletionRoutine: LPWSAOVERLAPPED_COMPLETION_ROUTINE
+): cint {.libWs2_32, importc: "WSARecv".}
+
+proc WSAGetLastError*(): cint {.libWs2_32, importc: "WSAGetLastError".}
 
 when isMainModule:
   echo createIoCompletionPort(INVALID_HANDLE_VALUE, 0, 0, 1)
